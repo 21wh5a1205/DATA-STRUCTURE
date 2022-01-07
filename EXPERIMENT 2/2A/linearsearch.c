9f81ee96e9f7c2b0e0006e408ea647d6007efced
21wh5a1205 <93641249+21wh5a1205@users.noreply.github.com>
2022-01-07 15:48:42 +0530

    Add files via upload

---
+++ EXPERIMENT 2/2A/linearsearch.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int linear_search(int a[],int n,int key);
+
+int main(){
+         int a[10];
+         int i,n,pos,key;
+         printf("enter the value of n");
+         scanf("%d",&n);
+         printf("enter the elements");
+         for(i=0;i<n;i++){
+            scanf("%d",&a[i]);
+            }
+         printf("enter the value uf key");
+         scanf("%d",&key);
+            
+         pos=linear_search(a,n,key);
+         if(pos>=0)
+         printf("element is found at %d position",pos);
+         else
+         printf("element not found");
+         return 0;
+   }
+   
+int linear_search(int a[],int n,int key){
+        int i;
+        for(i=0;i<n;i++){
+          if(a[i]==key)
+            return i;
+         }
+          return -1;
+          
+          
+}
+          
+          
+
